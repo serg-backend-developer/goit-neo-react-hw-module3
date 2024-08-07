b3e0f8d20ca0418d960e56b954df6208fc3903fb
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-08-07 10:17:54 +0200

    Contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,22 @@
+import { FaUser, FaPhone } from 'react-icons/fa6';
+
+import css from './Contact.module.css';
+
+const Contact = ({ id, name, number, onDelete }) => {
+  return (
+    <li className={css.contact}>
+      <div className={css.contacts}>
+        <div>
+          <FaUser /> {name}
+        </div>
+        <div>
+          <FaPhone /> {number}
+        </div>
+      </div>
+      <button type="button" onClick={() => onDelete(id)}>
+        Delete
+      </button>
+    </li>
+  );
+};
+export default Contact;
